7849865c69043ed7eda93ff034a7f01de9f89c1b
dtmhdev89-w <dinh.tran.minh.hieu@sun-asterisk.com>
2021-02-03 17:43:29 +0700

    Turorial taniarascia add hello

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import './css/main.css';
+import Table from './Table';
 
 function App() {
+  const characters = [
+    {
+      name: 'Charlie Hieu',
+      job: 'Janitor',
+    },
+    {
+      name: 'Mac',
+      job: 'Bouncer',
+    },
+    {
+      name: 'Dee',
+      job: 'Aspring actress',
+    },
+    {
+      name: 'Dennis',
+      job: 'Bartender',
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="site-heading">Hello, React!</h1>
+      <div className="container">
+        <Table characterData={characters} />
+      </div>
     </div>
   );
 }
